ae502ce1a20cab6fae828e241c4bc4d1dc5ffb33
dlwfldjf44 <dlwmfldjf44@naver.com>
2023-05-29 19:41:18 +0900

    first commit

---
+++ pratice.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="pratice.css">
+    <link href="https://fonts.googleapis.com/css2?family=Cherry+Bomb+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cherry+Bomb+One&family=Sunflower:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div id="logo">
+        <img src="elephant_coin.png" class="elephant_coin">
+        <div id="login_help">
+            <span class="elephant"><a href="pratice.php"><h2>ELEPHANT</h2></a></span>
+            <div id="id">
+            <input type="text" class="user_id" name="user_id" placeholder="아이디를 입력하세요">
+            </div>
+            <div id="password">
+            <input type="text" class="user_pass" name="user_pass" placeholder="비밀번호를 입력하세요">
+            </div>
+            <div id="submit">
+                <button name="login_btn" class="submit">로그인</button>
+            </div>
+            <div id="register">
+                <a href="#"><p>아직 회원이 아니신가요?</p></a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ register_server.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="register_server.php" method="post">
+        <div id="register">
+            <div id="id">
+                <label>아이디</label>
+                <input type="text" name="user_id" class="user_id" placeholder="아이디">
+            </div>
+            <div id="id">
+                <label>비밀번호</label>
+                <input type="text" name="user_id" class="user_pass" placeholder="아이디">
+            </div>
+            <div id="id">
+                <label>이름</label>
+                <input type="text" name="user_id" class="user_name" placeholder="아이디">
+            </div>
+            <div id="id">
+                <label>닉네임</label>
+                <input type="text" name="user_id" class="user_nick" placeholder="아이디">
+            </div>
+            <div id="id">
+                <label>핸드폰</label>
+                <input type="text" name="user_id" class="user_phone" placeholder="아이디">
+            </div>
+            <div id="id">
+                <label>계좌번호</label>
+                <input type="text" name="user_id" class="user_banknum" placeholder="아이디">
+            </div>
+            <div id="id">
+                <label>은행명</label>
+                <input type="text" name="user_id" class="user_bank" placeholder="아이디">
+            </div>
+        </div>
+    </form>
+</body>
+</html>
